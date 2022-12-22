9a42c1230e367f5cefcac94ca61080173dc9e2d7
dqueiros <diegoqueiros@hotmail.com>
2022-12-22 18:11:11 -0300

    adicionado o arquivo desafioCap02

---
+++ cap02/desafio/desafioCap02.php
@@ -0,0 +1,31 @@
+<?php
+  $month_portuguese = array(
+    'Jan' => 'Janeiro',
+    'Feb' => 'Fevereiro',
+    'Mar' => 'Março',
+    'Apr' => 'Abril',
+    'May' => 'Maio',
+    'Jun' => 'Junho',
+    'Jul' => 'Julho',
+    'Aug' => 'Agosto',
+    'Sep' => 'Setembro',
+    'Nov' => 'Novembro',
+    'Out' => 'Outubro',
+    'Dec' => 'Dezembro'
+  );
+
+  $week_portuguese = array(
+    'Sun' => 'Domingo',
+    'Mon' => 'Segunda-Feira',
+    'Tue' => 'Terça-Feira',
+    'Wed' => 'Quarta-Feira',
+    'Thu' => 'Quinta-Feira',
+    'Fri' => 'Sexta-Feira',
+    'Sat' => 'Sábado'
+  );
+
+  $satuday = (date('w') - 6) * -1;
+  $satuday_phrase = $satuday == 0 ? "hoje é sábado" : "faltam " . $satuday . " para sábado.";
+
+  echo "Estamos em " . $month_portuguese[date('M')] . ", hoje é dia " . date("d/m/y") . ", " . $week_portuguese[date('D')] . ", e agora são " . date("H:i:s a") . ", " . $satuday_phrase
+?>
